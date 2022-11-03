2121c5f54ee71b334d62b98c02005fe43f6c3991
JFelipeBS <f.filipisilva13@hotmail.com>
2022-11-03 18:29:51 -0300

    final

--- src/main/java/com/genial/demo/DemoApplication.java
+++ src/main/java/com/genial/demo/DemoApplication.java
@@ -24,7 +24,7 @@ public class DemoApplication {
 	        return new WebMvcConfigurer(){
 	              @Override
 	              public void addCorsMappings(CorsRegistry registry){
-	               registry.addMapping("/**").allowedOrigins("*","http://localhost:8080");
+	               registry.addMapping("/**").allowedOrigins("*","http://localhost:3000");
 	                                                                     
 	             }                                                        
 	         };                                                           
