051304c479705d9c7899cc1d146789b0d6f6a4dc
JFelipeBS <f.filipisilva13@hotmail.com>
2022-11-08 11:45:49 -0300

    att

--- src/main/java/com/genial/demo/DemoApplication.java
+++ src/main/java/com/genial/demo/DemoApplication.java
@@ -4,8 +4,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -19,15 +17,6 @@ public class DemoApplication {
 		return new ModelMapper();
 	}
 
-	@Bean                  
-	      public WebMvcConfigurer corsConfigurer(){
-	        return new WebMvcConfigurer(){
-	              @Override
-	              public void addCorsMappings(CorsRegistry registry){
-	               registry.addMapping("/**").allowedOrigins("*","http://localhost:3000");
-	                                                                     
-	             }                                                        
-	         };                                                           
-	      }   
+	
 
 }
